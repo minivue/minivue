3137fe989ed5eebdbea4e1f1a425cdd670458305
weifeiyue <weifeiyue@wps.cn>
2025-01-21 17:48:15 +0800

    update

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,11 +2,10 @@ import { cac } from 'cac'
 import { version } from '../package.json'
 import minivue from '@minivue/compiler'
 import { basename, join } from "path"
-import { build } from "esbuild"
+import { BuildOptions, build, context } from "esbuild"
 import fg from "fast-glob"
 
 const files = fg.sync(["pages/**/*.vue", "components/**/*.vue"])
-
 const entrys = Object.fromEntries(
   files.map((item) => {
     const fileName = basename(item, ".vue");
@@ -15,36 +14,41 @@ const entrys = Object.fromEntries(
   })
 );
 
+const config: BuildOptions = {
+  entryPoints: {
+    app: "app.vue",
+    ...entrys,
+  },
+  bundle: true,
+  outdir: "dist",
+  format: "esm",
+  sourcemap: true,
+  minify: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+  splitting: true,
+  treeShaking: true,
+  define: {
+    __DEV__: "false",
+    __MINIVUE__: "true",
+  },
+  plugins: [
+    minivue()
+  ],
+}
+
 const cli = cac('minivue')
+
+cli.command('dev', 'dev your app').action(async () => {
+  const ctx = await context(config)
+  await ctx.watch()
+  console.log('watching...')
+})
 
 cli.command('build', 'build your app').action(async () => {
   console.log('building...')
-  await new Promise((resolve) => {
-    setTimeout(resolve, 5000)
-  })
-  await build({
-    entryPoints: {
-      app: "app.vue",
-      ...entrys,
-    },
-    bundle: true,
-    outdir: "dist",
-    format: "esm",
-    sourcemap: true,
-    minify: true,
-    minifyIdentifiers: true,
-    minifySyntax: true,
-    minifyWhitespace: true,
-    splitting: true,
-    treeShaking: true,
-    define: {
-      __DEV__: "false",
-      __MINIVUE__: "true",
-    },
-    plugins: [
-      minivue()
-    ],
-  })
+  await build(config)
 })
 
 cli.help()
