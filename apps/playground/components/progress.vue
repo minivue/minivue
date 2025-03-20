70425d922d443c34b61b4520b7ea620e7ca294d4
weifeiyue <weifeiyue@wps.cn>
2025-03-20 21:24:23 +0800

    chore: update

---
+++ apps/playground/components/progress.vue
@@ -0,0 +1,42 @@
+<template>
+  <View :class="classes" :style="styles"></View>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from '@minivue/core'
+import { classnames } from './utils'
+
+defineOptions({
+  name: 'KdProgress',
+})
+
+const percentage = ref(110)
+
+const classes = computed(() => classnames('kd-progress'))
+
+const styles = computed(
+  () =>
+    `background-image: url("data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg' fill='none'%3E%3Ccircle r='17.5' cx='20' cy='20' stroke='%230D0D0D' stroke-width='5' stroke-opacity='.12'/%3E%3Ccircle r='17.5' cx='20' cy='20' stroke='%231F69E0' stroke-width='5' stroke-linecap='round' stroke-dashoffset='${percentage.value}' fill='transparent' stroke-dasharray='110'/%3E%3C/svg%3E")`,
+)
+
+setInterval(() => {
+  percentage.value -= 1
+}, 100)
+</script>
+
+<style>
+.kd-progress {
+  display: inline-flex;
+  flex-shrink: 0;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  vertical-align: middle;
+}
+</style>
+
+<config lang="json">
+{
+  "component": true
+}
+</config>
--- apps/playground/pages/index.vue
+++ apps/playground/pages/index.vue
@@ -1,98 +1,12 @@
 <template>
   <KdPage>
-    <View style="height: 60px"></View>
-    <View style="background-color: #ccc">
-      <KdLoading text="加载中…" size="s" />
-      <KdLoading text="加载中…" size="m" />
-      <KdLoading text="加载中…" size="l" />
-      <KdLoading text="加载中…" mode="dark" />
-      <KdLoading text="加载中…" size="s" vertical />
-      <KdLoading text="加载中…" size="m" vertical />
-      <KdLoading text="加载中…" size="l" vertical />
-    </View>
-    <KdDivider />
-    <KdDivider vertical style="height: 50px; margin-left: 100px" />
-    <View>
-      <KdButton type="secondary" icon="deepseek" only-icon></KdButton>
-      <KdButton type="primary" size="l">主要按钮</KdButton>
-      <KdButton type="primary" size="xl" disabled dropdown>主要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="primary" ai dropdown>主要按钮</KdButton>
-      <KdButton type="primary" size="l" loading ai>主要按钮</KdButton>
-      <KdButton type="primary" size="xl" disabled dropdown ai>主要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="primary" danger dropdown>主要按钮</KdButton>
-      <KdButton type="primary" size="l" loading danger>主要按钮</KdButton>
-      <KdButton type="primary" size="xl" disabled danger dropdown>主要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton>次要按钮</KdButton>
-      <KdButton size="l">次要按钮</KdButton>
-      <KdButton size="xl" loading>次要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton highlight dropdown>次要按钮</KdButton>
-      <KdButton size="l" highlight dropdown>次要按钮</KdButton>
-      <KdButton size="xl" highlight dropdown>次要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="light" dropdown>轻浅按钮</KdButton>
-      <KdButton type="light" size="l" dropdown>轻浅按钮</KdButton>
-      <KdButton type="light" size="xl" dropdown>轻浅按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="light" highlight dropdown>轻浅按钮</KdButton>
-      <KdButton type="light" size="l" highlight dropdown>轻浅按钮</KdButton>
-      <KdButton type="light" size="xl" highlight dropdown>轻浅按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="light" danger dropdown>轻浅按钮</KdButton>
-      <KdButton type="light" size="l" danger dropdown>轻浅按钮</KdButton>
-      <KdButton type="light" size="xl" danger dropdown>轻浅按钮</KdButton>
-    </View>
-    <View>
-      <KdButton dropdown>次要按钮</KdButton>
-      <KdButton size="l" dropdown>次要按钮</KdButton>
-      <KdButton size="xl" dropdown>次要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="light" icon="deepseek" vertical>主要按钮</KdButton>
-      <KdButton type="secondary" icon="deepseek" size="l" vertical>主要按钮</KdButton>
-      <KdButton type="light" icon="deepseek" size="xl" vertical loading>主要按钮</KdButton>
-    </View>
-    <View>
-      <KdButton type="light" icon="deepseek" vertical dropdown>主要按钮</KdButton>
-      <KdButton type="secondary" icon="deepseek" vertical dropdown>主要按钮</KdButton>
-      <KdButton type="light" icon="deepseek" vertical loading dropdown>主要按钮</KdButton>
-    </View>
-    <KdButtonGroup>
-      <KdButton size="xl">次要按钮</KdButton>
-      <KdButton type="primary" size="xl">次要按钮</KdButton>
-    </KdButtonGroup>
-    <KdButtonGroup vertical>
-      <KdButton type="primary" size="xl">次要按钮</KdButton>
-      <KdButton size="xl">次要按钮</KdButton>
-    </KdButtonGroup>
-    <KdButtonGroup>
-      <KdButton size="l">次要按钮</KdButton>
-      <KdButton size="l">次要按钮</KdButton>
-      <KdButton type="primary" size="l">次要按钮</KdButton>
-    </KdButtonGroup>
-    <KdButtonGroup vertical>
-      <KdButton type="primary" size="l">次要按钮</KdButton>
-      <KdButton size="l">次要按钮</KdButton>
-    </KdButtonGroup>
+    <KdProgress />
   </KdPage>
 </template>
 
 <script setup lang="ts">
 import KdPage from '@/components/page.vue'
-import KdButton from '@/components/button.vue'
-import KdLoading from '@/components/loading.vue'
-import KdButtonGroup from '@/components/button-group.vue'
-import KdDivider from '@/components/divider.vue'
+import KdProgress from '@/components/progress.vue'
 </script>
 
 <config lang="json">
