7616840cfd76c92933bc387364b5c305fe36fe63
weifeiyue <weifeiyue@wps.cn>
2025-05-12 18:55:59 +0800

    chore: update

--- apps/playground/pages/bdage.vue
+++ apps/playground/pages/bdage.vue
@@ -1,8 +1,40 @@
 <template>
   <KdPage title="徽标组件">
-    <View class="p-badge">
-      有消息
-      <KdBdage content="1" ellipsis />
+    <View class="p-badge__box">
+      <View class="p-badge__item">
+        有消息
+        <KdBdage />
+      </View>
+    </View>
+    <View class="p-badge__box">
+      <View class="p-badge__item">
+        有消息
+        <KdBdage content="1" />
+      </View>
+    </View>
+    <View class="p-badge__box">
+      <View class="p-badge__item">
+        有消息
+        <KdBdage content="99" />
+      </View>
+    </View>
+    <View class="p-badge__box">
+      <View class="p-badge__item">
+        有消息
+        <KdBdage content="99+" />
+      </View>
+    </View>
+    <View class="p-badge__box">
+      <View class="p-badge__item">
+        有消息
+        <KdBdage content="NEW" />
+      </View>
+    </View>
+    <View class="p-badge__box">
+      <View class="p-badge__item">
+        有消息
+        <KdBdage content="1" ellipsis />
+      </View>
     </View>
   </KdPage>
 </template>
@@ -13,7 +45,11 @@ import KdPage from '@/components/page.vue'
 </script>
 
 <style>
-.p-badge {
+.p-badge__box {
+  padding: 16px;
+}
+
+.p-badge__item {
   position: relative;
   display: inline-flex;
 }
